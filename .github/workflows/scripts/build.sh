43a030be3390f0972b9c5e0a5b1b1ca196365a6d
samiura <samiura@splunk.com>
2023-02-03 17:42:15 -0800

    fix

---
+++ .github/workflows/scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+
+docker run \
+  -it --rm \
+  --mount "type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock" \
+  --mount "type=bind,source=$(git rev-parse --show-toplevel),destination=/root/src,readonly" \
+  --env EBPF_NET_SRC=/root/src \
+  --env EBPF_NET_OUT_DIR=/root/out \
+  --workdir=/root/out \
+  build-env \
+    ../build.sh docker

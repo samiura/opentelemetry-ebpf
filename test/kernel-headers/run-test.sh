b9aea5675ba6f2bf1bec2a35ec3bb2f53203aa56
samiura <samiura@splunk.com>
2023-02-21 18:56:17 -0800

    First working version

--- test/kernel-headers/run-test.sh
+++ test/kernel-headers/run-test.sh
@@ -8,11 +8,6 @@ export EBPF_NET_SRC_ROOT="${EBPF_NET_SRC_ROOT:-$(git rev-parse --show-toplevel)}
 source "${EBPF_NET_SRC_ROOT}/dev/script/bash-error-lib.sh"
 set -x
 
-
-
-
-
-
 function print {
   echo "===================================== $1 ====================================="
 }
